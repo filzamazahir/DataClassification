e299ec3e54c9d86968c594d3a263cfb4b869c92d
Filza Mazahir <filza.mazahir@gmail.com>
2018-02-21 09:34:26 -0500

    Load dataset

---
+++ TreesOfPredictors.py
@@ -0,0 +1,6 @@
+# Trees of Predictors (ToPs) ensemble learning method
+# Source: https://arxiv.org/abs/1706.01396v2
+
+# Project - ECE657A  (Group __)  Filza Mazahir 20295951  &  Tarneem Barayyan 20645942 
+
+
--- dataclassification_project_ece657a.py
+++ dataclassification_project_ece657a.py
@@ -19,9 +19,49 @@ import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
-# 1. Load the online news popularity dataset and store it as a pandas dataframe
+# Load the online news popularity dataset and store it as a pandas dataframe
 file_location = 'OnlineNewsPopularity.csv'
-news_df = pd.read_csv(file_location)
+news_df_original = pd.read_csv(file_location, sep=', ', engine='python')
+
+
+# Data Preprocessing
+
+# Drop non-predictive attributes
+news_df = news_df_original.drop(['url', 'timedelta'], axis = 1) 
+
+# Check for outliers/noise/NaN on news_df
+
+
+
+
+# Getting dataset ready
+news_y = news_df['shares']
+news_y = news_y.apply(lambda x: 1 if x>=1400 else 0)
+news_x = news_df.drop(['shares'], axis = 1)
+
+
+# Split dataset into test and train set - 25% (9911 instances out of 39644) used for testing
+news_x_train, news_x_test, news_y_train, news_y_test = train_test_split(news_x, news_y, test_size=0.25, random_state=42)
+
+
+# Random Forest Classifier
+
+
+
+
+# Extra Trees Classifier
+
+
+
+
+# AdaBoost Classifier
+
+
+
+
+# Trees of Predictors Classifier
+
+
 
 
 
